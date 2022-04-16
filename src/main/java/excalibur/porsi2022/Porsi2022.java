61fd9595467f4738b8b8b256cfae2ab84e165632
Hersa37 <bernardus.hersa37@gmail.com>
2022-04-16 20:18:50 +0700

    First commit

---
+++ src/main/java/excalibur/porsi2022/Porsi2022.java
@@ -0,0 +1,17 @@
+/*
+ *  Made for college assignments/personal projects.
+ *  Do not use without permission
+ */
+
+package excalibur.porsi2022;
+
+/**
+ *
+ * @author echa
+ */
+public class Porsi2022 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
